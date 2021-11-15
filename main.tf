35b0decb99d48b82a63f5ced582abaee93e193b8
Jens Wegar <jens.wegar@locotech.fi>
2021-11-15 14:48:55 +0200

    fix: syntax errors in tf template

--- main.tf
+++ main.tf
@@ -40,10 +40,10 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name = azurerm_resource_group.labnosec.name
   account_tier        = "Standard"
   account_replication_type = "LRS"
-  enable_https_traffic_only: true
-  network_rules: [
+  enable_https_traffic_only = true
+  network_rules = [
     {
-      "bypass": "AzureServices"
+      "bypass" = "AzureServices"
     }
   ]
 }
